6d01f2f6721626abcd4e52ddee4632843e8632cd
logrcia <lolagm04@gmail.com>
2025-11-19 22:28:28 -0300

    margin about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import AboutContainer from "@/components/AboutContainer"
 import Navbar from "@/components/Navbar"
 const about = () => {
   return (
-    <div className="bg-linear-to-br from-pink-100 to-pink-200">
+    <div className="bg-linear-to-br from-pink-100 to-pink-200 mt-16">
     <Navbar/>
     <AboutContainer/>
     </div>
